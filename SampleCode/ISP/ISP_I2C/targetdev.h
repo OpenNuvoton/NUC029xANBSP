7d7e93fd4b51e585c73c93c3a3132562372cc92f
SFWU <SFWU@nuvoton.com>
2019-09-26 13:59:26 +0800

    Add ISP_I2C sample code.
    Change-Id: Ib6a9161747dbef0e7c2b926383c3d8866cbf0b27

---
+++ SampleCode/ISP/ISP_I2C/targetdev.h
@@ -0,0 +1,13 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ * @version  0x32
+ *
+ * @note
+ * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include "NUC029xAN.h"
+#include "isp_user.h"
+
+
+/*** (C) COPYRIGHT 2019 Nuvoton Technology Corp. ***/
