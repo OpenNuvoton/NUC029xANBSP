4b4a5fb4783a03c82343a50754eacdd61148b76c
WHHuang1_nuvoton <WHHuang1@nuvoton.com>
2024-05-27 16:09:22 +0800

    Set [clear ADF flag operation] to the first operation in ADC_IRQHandler.
    Change-Id: I54cf4eef77f5c592daf59aca70531755f67ca15e

--- SampleCode/StdDriver/ADC_BurstMode/main.c
+++ SampleCode/StdDriver/ADC_BurstMode/main.c
@@ -248,6 +248,8 @@ void AdcBurstModeTest()
 /*---------------------------------------------------------------------------------------------------------*/
 void ADC_IRQHandler(void)
 {
+    ADC_CLR_INT_FLAG(ADC, ADC_ADF_INT); /* clear the A/D conversion flag */
+    
     while(ADC_IS_DATA_VALID(ADC, 0)) /* Check the VALID bits */
     {
         if(g_u32AdcDataCount >= BURST_COUNT)
@@ -255,8 +257,6 @@ void ADC_IRQHandler(void)
         /* In burst mode, the software always gets the conversion result of the specified channel from channel 0 */
         g_au16AdcData[g_u32AdcDataCount++] = (uint16_t)ADC_GET_CONVERSION_DATA(ADC, 0);
     }
-
-    ADC_CLR_INT_FLAG(ADC, ADC_ADF_INT); /* clear the A/D conversion flag */
 }
 
 /*---------------------------------------------------------------------------------------------------------*/
